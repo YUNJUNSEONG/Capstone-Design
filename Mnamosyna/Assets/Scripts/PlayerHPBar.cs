253ecc17668ffbffef3ef8459c26d42b4877ec5c
윤준성 <117891079+YUNJUNSEONG@users.noreply.github.com>
2024-04-17 09:46:07 +0900

    체력/스테미나 ui 변경 및 적용

--- Mnamosyna/Assets/Scripts/PlayerHPBar.cs
+++ Mnamosyna/Assets/Scripts/PlayerHPBar.cs
@@ -2,16 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI;
 
 public class PlayerHPBar : MonoBehaviour
 {
     public PlayerStat stat;
     public Image hpImage; 
     public Image stImage;
-    
+    private static readonly int FillLevel = Shader.PropertyToID("_FillLevel");
+
+
     void Update()
     {
-        hpImage.fillAmount = (float)stat.Cur_HP / stat.Max_HP;
-        stImage.fillAmount = ((float)stat.Cur_Stamina / stat.Max_Stamina);
+        UpdateHPValue((float)stat.Cur_HP / stat.Max_HP);
+        UpdateStaminaValue(((float)stat.Cur_Stamina / stat.Max_Stamina));
+    }
+
+    void UpdateHPValue(float value)
+    {
+        if (hpImage == null)
+        {
+            return;
+        }
+        hpImage.material.SetFloat(FillLevel, value);
+    }
+
+    void UpdateStaminaValue(float value)
+    {
+        if (stImage == null)
+        {
+            return;
+        }
+        stImage.material.SetFloat(FillLevel, value);
     }
 }
