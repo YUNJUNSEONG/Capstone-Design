75484985dd4b0f7d142f33fcbcd2073f3ee63e6a
parkjuyun <50567352+parkjuyun@users.noreply.github.com>
2024-05-04 05:25:01 +0900

    카메라 destination tag추가 완료
    안개 에셋 켰다 꺼기 및 카메라 destination 까지 일직선 움직임 구현 필요 //
    게임 시작 버튼, 로고 기능 연결 필요

--- Mnamosyna/Assets/Scripts/CameraMove.cs
+++ Mnamosyna/Assets/Scripts/CameraMove.cs
@@ -10,11 +10,21 @@ public class CameraMove: MonoBehaviour
     private bool _isCameraEventEnded = false;
     private float _pos_v = 0f;
     // Update is called once per frame
+    private Quaternion initialRotation; //초기 카메라 셋팅값 저장 하기 위해
+
+    void start()
+    {
+        //초기 카메라 셋팅값 저장하기 위해 / destination tag 찾기 실행
+        initialRotation = transform.rotation;
+        destination = GameObject.FindGameObjectWithTag("destination");
+    }    
     void Update()
     {
         if (_isCameraEventEnded)
             CameraEvent();
         else
+            //초기 카메라 셋팅 값 가져오기
+            transform.rotation = initialRotation;
             transform.position = new Vector3(target.transform.position.x + offset.x, offset.y, target.transform.position.z + offset.z);
     }
 
@@ -22,11 +32,16 @@ public class CameraMove: MonoBehaviour
     {
         //1. 안개 에셋이 카메라 이벤트 동안 켜졌다 꺼지기
         //2. destination 값받아서 일직선으로 움직였다가 다시 돌아오기
-        //3. 되돌아오면 _isCameraEventEnded 값 넘겨주기
-        //Q1. 시작 카메라 각도를 변경하고 싶은데, -> 초기화 x,y,z 값으로 진행했다가 다시 else진입시 main camera setting값 받아올 수 있는지? (tmp저장했다가)
         if(_pos_v < 10)
-            _pos_v += Time.deltaTime;
-
-        transform.position = new Vector3();
+        {
+             _pos_v += Time.deltaTime;
+        //transform.position = new Vector3();
+        }
+        //else에서 _isCameraEventEnded 값 넘겨주기
+        else
+        {
+            _isCameraEventEnded = true;
+            _pos_v = 0f;
+        }
     }
 }
