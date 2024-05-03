f7ff04ab5b1aab1417353c8a9b24c073ce0a3e19
parkjuyun <culcul7164@gmail.com>
2024-05-03 17:37:42 +0900

    JY

--- Mnamosyna/Assets/Scripts/CameraMove.cs
+++ Mnamosyna/Assets/Scripts/CameraMove.cs
@@ -6,9 +6,22 @@ public class CameraMove: MonoBehaviour
 {
     public GameObject target;
     public Vector3 offset;
+    public GameObject destination;
+    private bool _isCameraEventEnded = false;
+    private float _pos_v = 0f;
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(target.transform.position.x + offset.x, offset.y, target.transform.position.z + offset.z);
+        if (_isCameraEventEnded)
+            CameraEvent();
+        else
+            transform.position = new Vector3(target.transform.position.x + offset.x, offset.y, target.transform.position.z + offset.z);
+    }
+
+    void CameraEvent()
+    {
+        if(_pos_v < 10)
+            _pos_v += Time.deltaTime;
+
     }
 }
