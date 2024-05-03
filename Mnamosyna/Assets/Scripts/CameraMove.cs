b9df93a35aa8911233c72f85b7d213d9dc4fbfc6
parkjuyun <50567352+parkjuyun@users.noreply.github.com>
2024-05-04 04:05:45 +0900

    fog asset 추가
    height fog asset 추가 /
    CameraMove.cs 온로드 진행 시 보스맵 obj까지 훑고 진행되도록 코드 수정 진행 중

--- Mnamosyna/Assets/Scripts/CameraMove.cs
+++ Mnamosyna/Assets/Scripts/CameraMove.cs
@@ -20,8 +20,13 @@ public class CameraMove: MonoBehaviour
 
     void CameraEvent()
     {
+        //1. 안개 에셋이 카메라 이벤트 동안 켜졌다 꺼지기
+        //2. destination 값받아서 일직선으로 움직였다가 다시 돌아오기
+        //3. 되돌아오면 _isCameraEventEnded 값 넘겨주기
+        //Q1. 시작 카메라 각도를 변경하고 싶은데, -> 초기화 x,y,z 값으로 진행했다가 다시 else진입시 main camera setting값 받아올 수 있는지? (tmp저장했다가)
         if(_pos_v < 10)
             _pos_v += Time.deltaTime;
 
+        transform.position = new Vector3();
     }
 }
