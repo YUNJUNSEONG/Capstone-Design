ebaf7006e83a398942a92f7f73bfe5bb6c182853
seotaerin <taerin.j.s@gmail.com>
2024-05-22 23:05:47 +0900

    magic0스크립트에 이동좌표 설정

--- Mnamosyna/Assets/Scripts/_Map/Magic0.cs
+++ Mnamosyna/Assets/Scripts/_Map/Magic0.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Magic0 : MonoBehaviour
 {
     Collider myCollider;
-    //Renderer myRenderer;
     ParticleSystem myParticleSystem;
     
     void Awake()
@@ -37,7 +36,20 @@ public class Magic0 : MonoBehaviour
         if (this.name == "Magic-2to3_2") {collision.transform.position = new Vector3(10.58f, 1.27f, 238.42f);}
         if (this.name == "Magic-2to3_3") {collision.transform.position = new Vector3(10.58f, 1.27f, 238.42f);}
         
-        if (this.name == "Magic-3") {collision.transform.position = new Vector3(-40.89f, 1.27f, 334.344f);}
+        if (this.name == "Magic-3") {collision.transform.position = new Vector3(-40.89f, 1.27f, 334.344f);}//3-1~3->heal
+        
+        //포탈 오브젝트 아래에 있는 portal blue, portal yellow를 맞는 이름으로 바꿔야함 
+        if (this.name == "Magic-healto4") {collision.transform.position = new Vector3(-28.84f, 1.27f, 407.64f);} //heal->4-1
+        
+        if (this.name == "Magic-4to5_1") {collision.transform.position = new Vector3(-98.1f, 1.27f, 493f);} //4-1->5-1
+        if (this.name == "Magic-4to5_2") {collision.transform.position = new Vector3(55.39f, 1.27f, 488.57f);} //4-1->5-2
+        
+        if (this.name == "Magic-5to6_1") {collision.transform.position = new Vector3(-110.8f, 1.27f, 580.94f);} //5-1->6-1
+        if (this.name == "Magic-5to6_2") {collision.transform.position = new Vector3(83.25f, 1.27f, 547.61f);} //5-1~2->6-2
+        if (this.name == "Magic-5to6_3") {collision.transform.position = new Vector3(6.86f, 1.27f, 684.92f);} //5-2->6-3
+        
+        if (this.name == "Magic-6toBoss") {collision.transform.position = new Vector3(18.56f, 1.27f, 793.5f);} //6-1~3->boss
+        
 
         MonsterSpawnEffect monsterSpawnEffect = collision.gameObject.GetComponent<MonsterSpawnEffect>();
         if (monsterSpawnEffect != null) {monsterSpawnEffect.StartSpawnEffect();}
