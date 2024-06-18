f9d4b236df7982600037b713016512cc1adb937a
YUNJUNSEONG <117891079+YUNJUNSEONG@users.noreply.github.com>
2024-06-18 18:32:16 +0900

    .

--- Mnamosyna/Assets/Scripts/Story/BossTuto.cs
+++ Mnamosyna/Assets/Scripts/Story/BossTuto.cs
@@ -13,7 +13,7 @@ public class BossTuto : MonoBehaviour
             List<string> FirstBossTutorialMessages = new List<string>
             {
                 "여기는 이 구역을 지키는 사룡의 수하가 있을 거예요.",
-                "저길 보세요.\r\n저건 제가 이 구역의 주인인 드래곤이예요.",
+                "저길 보세요.\r\n저건 이 구역의 주인인 드래곤이예요.",
                 "사룡보다는 약하더라도 성체인 드래곤은 매우 강해요.",
                 "지금까지 상대해왔던 해츨링과는 차원이 다른거예요.",
                 "그러니 조심해서 상대하죠."
--- Mnamosyna/Assets/Scripts/Story/SkillTuto.cs
+++ Mnamosyna/Assets/Scripts/Story/SkillTuto.cs
@@ -13,11 +13,11 @@ public class SkillTuto : MonoBehaviour
         {
             List<string> UnlockExplanTutorialMessages = new List<string>
             {
-                "당신이 선택하는 기술의 기억에 따라 \r\n 무기가 바뀌고 얻을 수 있는 기술들이 달라질거예요.",
-                "당신의 첫번째 기억은 무엇인가요? \r\n 어떤 무기를 사용했죠?",
-                "앞으로 기억을 되찾으면서 얻은 기술들은 공격과 회피를 조합하여 사용할 수 있는 콤보 기술,",
+                "당신이 선택하는 기술의 기억에 따라 \r\n무기가 바뀌고 얻을 수 있는 기술들이 달라질거예요.",
+                "당신의 첫번째 기억은 무엇인가요? \r\n어떤 무기를 사용했죠?",
+                "앞으로 기억을 되찾으면서 얻은 기술들은 \r\n공격과 회피를 조합하여 사용할 수 있는 콤보 기술,",
                 "스스로를 강화 시켜주는 패시브를 얻을 수 있을 거예요.",
-                "자신에게 필요한게 무엇일지 선택하면서 \r\n 자, 앞으로 나아가죠.",
+                "자신에게 필요한게 무엇일지 선택하면서 \r\n자, 앞으로 나아가죠.",
             };
             storyManager.StartTutorial(StoryManager.TutorialType.SkillUnlock, UnlockExplanTutorialMessages);
 
