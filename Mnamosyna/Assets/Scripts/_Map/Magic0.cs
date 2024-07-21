3845a866b6de48922690c5017533e285789a94b3
seotaerin <taerin.j.s@gmail.com>
2024-07-21 22:35:36 +0900

    2스테이지 완-콜라이더, 네비메쉬, 포탈좌표입력 완

--- Mnamosyna/Assets/Scripts/_Map/Magic0.cs
+++ Mnamosyna/Assets/Scripts/_Map/Magic0.cs
@@ -15,8 +15,8 @@ public class Magic0 : MonoBehaviour
     
     void Start()
     {
-        if(myCollider) {myCollider.enabled = false;}
-        if(myParticleSystem) {myParticleSystem.Stop();}
+        //if(myCollider) {myCollider.enabled = false;}
+        //if(myParticleSystem) {myParticleSystem.Stop();}
     }
     
     public void EnableComponents()
@@ -50,6 +50,29 @@ public class Magic0 : MonoBehaviour
         
         if (this.name == "Magic-6toBoss") {collision.transform.position = new Vector3(18.56f, 1.27f, 793.5f);} //6-1~3->boss
         
+        
+        //2스테이지 포탈 좌표
+        if (this.name == "Magic-0to1") {collision.transform.position = new Vector3(11.91f, 4.7f, -46.59f);} //0->1
+        
+        if (this.name == "Magic-1to2_1") {collision.transform.position = new Vector3(-40.81f, 4.7f, -16.71f);} //1->2-1
+        if (this.name == "Magic-1to2_2") {collision.transform.position = new Vector3(-39.83f, 4.7f, 52.62f);} //1->2-2
+        
+        if (this.name == "Magic-2to3_1") {collision.transform.position = new Vector3(-127.49f, 4.7f, -48.1f);} //2->3-1
+        if (this.name == "Magic-2to3_2") {collision.transform.position = new Vector3(-109.33f, 4.7f, 19.2f);} //2->3-2
+        if (this.name == "Magic-2to3_3") {collision.transform.position = new Vector3(-101.21f, 4.7f, 80.89f);} //2->3-3
+        
+        if (this.name == "Magic-3toH") {collision.transform.position = new Vector3(-109.19f, 4.7f, 191.45f);} //3->Heal
+        
+        if (this.name == "Magic-Hto4") {collision.transform.position = new Vector3(-200.99f, 4.7f, 16.46f);} //Heal->4
+        
+        if (this.name == "Magic-4to5_1") {collision.transform.position = new Vector3(-283.81f, 4.7f, -13.8f);} //4->5-1
+        if (this.name == "Magic-4to5_2") {collision.transform.position = new Vector3(-255.68f, 4.7f, 54.65f);} //4->5-2
+        
+        if (this.name == "Magic-5to6_1") {collision.transform.position = new Vector3(-342.92f, 4.7f, -45.25f);} //5->6-1
+        if (this.name == "Magic-5to6_2") {collision.transform.position = new Vector3(-342.59f, 4.7f, 17.03f);} //5->6-2
+        if (this.name == "Magic-5to6_3") {collision.transform.position = new Vector3(-344.75f, 4.7f, 82.66f);} //5->6-3
+        
+        if (this.name == "Magic-6toB") {collision.transform.position = new Vector3(-201.89f, 4.7f, 190.98f);} //6->Boss
 
         MonsterSpawnEffect monsterSpawnEffect = collision.gameObject.GetComponent<MonsterSpawnEffect>();
         if (monsterSpawnEffect != null) {monsterSpawnEffect.StartSpawnEffect();}
